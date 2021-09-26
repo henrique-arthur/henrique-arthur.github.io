c092a26a3b7015b06c1c3b5f9afa16545c7d32f9
Arthur Henrique <arthur12souza@hotmail.com>
2021-09-26 12:19:57 -0300

    overflow desabilitado

--- action.js
+++ action.js
@@ -1,4 +1,5 @@
 function closeModal(){
   document.getElementById('ct').style.opacity= "100%";
   document.getElementById('md').style.display= "none";
+  document.getElementById('bdy').style.overflow="auto";
 }
