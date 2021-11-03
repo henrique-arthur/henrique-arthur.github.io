dcec58d8e5a1f1ecad599a24d04d224e60d9583e
Arthur Henrique <arthur12souza@hotmail.com>
2021-11-03 07:50:31 -0300

    primeira parte modo noturno

--- action.js
+++ action.js
@@ -3,3 +3,13 @@ function closeModal(){
   document.getElementById('md').style.display= "none";
   document.getElementById('bdy').style.overflow="auto";
 }
+
+function colorMode(){
+  if(document.documentElement.style.getPropertyValue("--lightmode-rightbg-color") ==="#f0f0f5"){
+    document.documentElement.style.setProperty("--lightmode-rightbg-color","#212121");
+    document.documentElement.style.setProperty("--lightmode-color","#f0f0f5");
+  }else{
+    document.documentElement.style.setProperty("--lightmode-rightbg-color","#f0f0f5");
+    document.documentElement.style.setProperty("--lightmode-color","#212121");
+  }
+}
